2c450f6c88ff09e67111dc3fe741c9cb20b082bf
Mateus Rodrigues <58078186+mtsrodrigues4991@users.noreply.github.com>
2022-07-06 20:36:09 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,18 +6,3 @@ const job = new CronJob('9 * * * * *', () => {        	//executa a tarefa a cada
 	console.log('Executando handler-min')		//escreve o texo que estiver entre ''
 	shell.exec('node handler-min.js');		//faz a execução do handler-min
 }, null, true, 'America/Sao_Paulo')
-
-/*
-const fs = require('fs');
-
-const data = 'Testando a criação de arquivos';
-
-const CronJob = require('cron').CronJob                 //para agendar a execução automática do envio de dados para outsystems
-const job = new CronJob('0 * * * * *', () => {          //executa a tarefa a cada 10 min
-
-fs.writeFile("teste.txt", data, (err) => {
-    if(err) { throw err;}
-    //console.log("Arquivo salvo");
-}); 
-}, null, true, 'America/Sao_Paulo')
-*/
